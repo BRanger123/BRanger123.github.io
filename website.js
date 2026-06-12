0cf3a0a864f6185cb2e8766ff049d2fac5cba0e9
Barnaby Ranger <20e33310@marling.school>
2026-06-12 08:10:12 +0000

    Coins added

--- website.js
+++ website.js
@@ -143,7 +143,8 @@ scene(1, () => {
 
 scene(2, () => {
     loadSprite("ghosty", "https://kaboomjs.com/sprites/ghosty.png")
-    loadSprite("boss", "https://kaboomjs.com/sprites/gigagantrum.png")
+    //loadSprite("boss", "https://kaboomjs.com/sprites/gigagantrum.png")
+    loadSprite("coin", "https://kaboomjs.com/sprites/coin.png")
     loadBean()
     setGravity(0)
     setBackground(rgb(255, 255, 255))
@@ -151,6 +152,7 @@ scene(2, () => {
     let isPaused = false
     let enemiesToKill = 10
     let enemiesToKillCounter = 0
+    let coins = 0
     
     // Experimental code for weapon class
     class gun{
@@ -166,11 +168,10 @@ scene(2, () => {
             this.recoilForce = recoilForce  // Force of recoil that pushes player back
         }
         
-        canFire() {
+        canFire(){
             return this.ammoInMag > 0
         }
-        
-        reload() {
+        reload(){
             if (this.totalAmmo > 0) {
                 const ammoToLoad = Math.min(this.magSize, this.totalAmmo)
                 this.totalAmmo -= ammoToLoad
@@ -179,9 +180,8 @@ scene(2, () => {
             }
             return false
         }
-        
         fireWeapon(){
-            if (!this.canFire()) {
+            if (!this.canFire()){
                 hintLabel.text = `Reload! (e)`
                 return  // Cannot fire if no ammo in magazine
             }
@@ -190,9 +190,9 @@ scene(2, () => {
             
             // Apply recoil - push player backwards opposite to aim direction
             const recoilDir = baseDir.scale(-this.recoilForce)
-            player.move(recoilDir)
+            player.recoil = player.recoil.add(recoilDir)
             
-            for (let i = 0; i < this.bulletsFired; i++) {
+            for (let i = 0; i < this.bulletsFired; i++){
                 const angle = baseDir.angle() + rand(-this.spread, this.spread)
                 const direction = Vec2.fromAngle(angle)
                 const bullet = add([
@@ -207,7 +207,8 @@ scene(2, () => {
                 bullet.onUpdate(() => {bullet.move(bullet.dir.scale(this.bulletSpeed))})
                 bullet.onCollide("enemy", (enemy) => {
                     spawnDamageNumber(enemy.pos, this.bulletDamage)
-                    enemy.hurt(this.bulletDamage)
+                    enemy.move(bullet.dir.scale(this.bulletSpeed))
+                    enemy.hurt(this.bulletDamage*2)
                     bullet.destroy()
                 })
             }
@@ -218,7 +219,7 @@ scene(2, () => {
     }
 
     // Function to spawn floating damage numbers
-    function spawnDamageNumber(position, damage) {
+    function spawnDamageNumber(position, damage){
         const damageText = add([
             text(damage.toString()),
             pos(position.x+Math.random()*20, position.y+Math.random()*20),
@@ -238,6 +239,17 @@ scene(2, () => {
         })
     }
 
+    function spawnCoin(xy){
+        const coin = add([
+            sprite("coin"),
+            anchor("center"),
+            pos(xy),
+            area({ collisionIgnore: ["enemy"]}),
+            body(),
+            "coin",
+        ])
+    }
+
     // Player code
     const player = add([
         sprite("bean"),
@@ -247,25 +259,39 @@ scene(2, () => {
         body(),
         health(100),
         "player",
-        { speed: 400 },
+        { speed: 400, recoil: vec2(0, 0) },
         offscreen({ destroy: true }),
     ])
+    
+    player.onCollide("coin", (coin) => {
+        destroy(coin)
+        coins=coins+1
+        coinsLabel.text = `Coins: ${coins}`
+    })
     // bulletSpeed, bulletColor, bulletDamage, magSize, bulletsFired, spread, recoilForce, totalAmmo
-    let gunTest = new gun(700, rgb(41, 41, 41), 20, 7, 7, 10, 1000, 100)
+    let gunTest = new gun(700, rgb(41, 41, 41), 20, 7, 7, 10, 2000, 100)
 
+    const coinsLabel = add([
+        text(`Coins: ${coins}`),
+        anchor("right"),
+        pos(width()-24, height()-100),
+        color(0, 0, 0),
+    ])
+    const enemiesDiedLabel = add([
+        text(`Kills: ${enemiesDied}`),
+        anchor("right"),
+        pos(width()-24, height()-50),
+        color(0, 0, 0),
+    ])
     const hintLabel = add([
         text(""),
-        pos(center().x-175, 24),
+        anchor("center"),
+        pos(center().x, 24),
         color(0, 0, 0),
     ])
     const ammoLabel = add([
         text(`Ammo: ${gunTest.ammoInMag}/${gunTest.totalAmmo}`),
         pos(24, height()-50),
-        color(0, 0, 0),
-    ])
-    const enemiesDiedLabel = add([
-        text(`Kills: ${enemiesDied}`),
-        pos(width()-240, height()-50),
         color(0, 0, 0),
     ])
     const healthLabel = add([
@@ -322,6 +348,7 @@ scene(2, () => {
                     player.hurt(20)
                 }
             }
+            spawnCoin(enemy.pos)
             destroy(enemy)
             burp()
             enemies.push(spawnEnemy())
@@ -340,13 +367,15 @@ scene(2, () => {
                 hintLabel.text = `Click to continue`
                 websiteGoTo('upgrade')
                 onClick(() => {
-                    isPaused = false
-                    hintLabel.text = ``
-                    if(upgradeValue==1){player.heal(100), healthLabel.text = `Health: ${player.hp()}`}
-                    if(upgradeValue==2){gunTest.bulletDamage += 5}
-                    if(upgradeValue==3){gunTest.magSize += 3, gunTest.totalAmmo += 3}
-                    if(upgradeValue==4){gunTest.totalAmmo += 50}
-                    upgradeValue=0
+                    if(upgradeValue!=0){
+                        isPaused = false
+                        hintLabel.text = ``
+                        if(upgradeValue==1){player.heal(100), healthLabel.text = `Health: ${player.hp()}`}
+                        if(upgradeValue==2){gunTest.bulletDamage += 5}
+                        if(upgradeValue==3){gunTest.magSize += 3, gunTest.totalAmmo += 3}
+                        if(upgradeValue==4){gunTest.totalAmmo += 50}
+                        upgradeValue=0
+                    }
                 })
             }
         })
@@ -359,7 +388,6 @@ scene(2, () => {
                 player.heal(5)
                 healthLabel.text = `Health: ${player.hp()}`
             }
-            // if(Math.random()*1 < 0.05){enemies.push(spawnBoss())}
             enemies.push(spawnEnemy())
         }
     })   // Time value acts as diffuculty
@@ -389,6 +417,16 @@ scene(2, () => {
         if (isKeyDown("s")) {dir.y = 1}
         const unitVec = dir.unit()
         player.move(unitVec.scale(player.speed))
+
+        if (player.recoil && player.recoil.len() > 0) {
+            const recoilDamping = 15
+            const recoilStep = player.recoil.scale(1 - Math.exp(-recoilDamping * dt()))
+            player.move(recoilStep)
+            player.recoil = player.recoil.sub(recoilStep)
+            if (player.recoil.len() < 1) {
+                player.recoil = vec2(0, 0)
+            }
+        }
     })
     onClick(() => {
         gunTest.fireWeapon()
@@ -410,10 +448,10 @@ scene(2, () => {
     //    camPos(player.pos)
     //})
 
-    onDestroy("player", () => go("deathScreen", enemiesDied))
+    onDestroy("player", () => go("deathScreen", enemiesDied*coins))
     player.on("death", () => {
         destroy(player)
-        go("deathScreen", enemiesDied)
+        go("deathScreen", enemiesDied*coins)
     })
 })
 
